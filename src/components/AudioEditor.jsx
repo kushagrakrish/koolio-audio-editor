b58fd8332aa86a6137833bfb082ed7fb2279891d
Kushagra Krishna <kushagrakrishna@Kushagras-MacBook-Air.local>
2023-08-16 14:33:21 +0530

    Zoom Slider implmented

--- src/components/AudioEditor.jsx
+++ src/components/AudioEditor.jsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
+import WaveSurfer from "wavesurfer.js";
 import { useWavesurfer } from "./waveHooks";
 
 const AudioEditor = (props) => {
   const containerRef = useRef();
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
+  const [zoom, setZoom] = useState(100); // Initialize zoom level to 100%
   const wavesurfer = useWavesurfer(containerRef, props);
 
   // On play button click
@@ -24,12 +26,23 @@ const AudioEditor = (props) => {
       wavesurfer.on("play", () => setIsPlaying(true)),
       wavesurfer.on("pause", () => setIsPlaying(false)),
       wavesurfer.on("timeupdate", (currentTime) => setCurrentTime(currentTime)),
+      wavesurfer.on("zoom", (minPxPerSec) => setZoom(minPxPerSec)),
     ];
 
     return () => {
       subscriptions.forEach((unsub) => unsub());
     };
   }, [wavesurfer]);
+
+  // Update the zoom level when the slider changes
+  const handleZoomSlider = (e) => {
+    // const newZoom = parseInt(e.target.value);
+    // setZoom(newZoom);
+    // wavesurfer.zoom(newZoom / 100);
+    const minPxPerSec = e.target.value;
+    // wavesurfer.zoom(minPxPerSec);
+    setZoom(wavesurfer.zoom(minPxPerSec));
+  };
 
   return (
     <div>
@@ -43,8 +56,20 @@ const AudioEditor = (props) => {
       </button>
 
       <p className='text-lg font-bold text-center mt-5'>
-        Seconds played: {currentTime}
+        Seconds played: {currentTime.toFixed(2)}
       </p>
+      <div>
+        <label>
+          Zoom:{" "}
+          <input
+            type='range'
+            min='10'
+            max='1000'
+            value={zoom}
+            onChange={handleZoomSlider}
+          />
+        </label>
+      </div>
     </div>
   );
 };
